3b8f12efe7e57f298c50fac9c1d8ec828469301b
Mertcan Zafer <117141770+mertcanzafer@users.noreply.github.com>
2023-06-01 20:05:50 +0300

    Update graphs.cpp

--- Graphs/graphs.cpp
+++ Graphs/graphs.cpp
@@ -55,10 +55,10 @@ void printGraph(vector<int> adjList[])
 
 	for (int i = 0; i < edges; i++)
 	{
-		//cin >> u >> v; // vertex u and v...
+		cin >> u >> v; // vertex u and v...
 	    
-		//adjMat[u][v] = 1;
-		//adjMat[v][u] = 1;
+		adjMat[u][v] = 1;
+		adjMat[v][u] = 1;
 	}
 
 	ShowAdjacencyMatrix(adjMat);
